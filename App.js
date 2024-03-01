d8c54ca3c14e0d4d113c6377cb71180fc6c1154a
Akash Patil <patilakash211@gmail.com>
2024-03-01 11:47:57 +0530

    added index and app.js and css page

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Im in h1 tag"),
+    React.createElement("h2", {}, "Im in h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Im in h1 tag"),
+    React.createElement("h2", {}, "Im in h2 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
